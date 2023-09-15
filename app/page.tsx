a09ccd22c642843fa50feb5a74647ae23ce0c1de
lu-perez <lu-perez@teco.com.ar>
2023-09-15 18:04:04 -0300

    header & dark-light themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <main className="grid place-items-center h-screen">
-      <Button>Click me</Button>
-
-      <Button
-        size='lg'
-        variant='destructive'
-      >Click me</Button>
-    </main>
+    <div>
+      Content
+    </div>
   )
 }
